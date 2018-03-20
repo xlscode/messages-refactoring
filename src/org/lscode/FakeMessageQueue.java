0f70da900be967daf1242179ae0aeba668a608db
xlscode <les@localhost>
2018-03-20 21:44:28 +0100

    Using enums instead of numbers as indexes for message fields. This is supposed to make dealing with the message fields easier due to meaningful names of enums (although the used names are not currently very meaningful). Some cleanup performed.

--- src/org/lscode/FakeMessageQueue.java
+++ src/org/lscode/FakeMessageQueue.java
@@ -1,10 +1,6 @@
 package org.lscode;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FakeMessageQueue implements Iterator<String>{
--- src/org/lscode/Main.java
+++ src/org/lscode/Main.java
@@ -1,22 +1,45 @@
 package org.lscode;
 
+
 public class Main {
     private static final int NUMOFPROBES = 15;
+
+    private enum MsgFld {
+        HEADER,
+        FIELD01,
+        FIELD02,
+        FIELD03,
+        FIELD04,
+        FIELD05,
+        FIELD06,
+        FIELD07,
+        FIELD08,
+        FIELD09,
+        FIELD10,
+        FIELD11,
+        FIELD12,
+        FIELD13,
+        FIELD14,
+        FIELD15,
+        FIELD16,
+        FIELD17
+    }
+
 
     public static void main(String[] args) {
         FakeMessageQueue mq = new FakeMessageQueue();
 
-        for (int i = 0; i<NUMOFPROBES; i++){
+        for (int i = 0; i < NUMOFPROBES; i++){
             String message = mq.next();
             String[] messageFields = message.split("\\|");
 
             switch (messageFields[0]){
                 case "AST":
                 case "ASL":
-                    some_method(messageFields[1], messageFields[5]);
+                    some_method(messageFields[MsgFld.FIELD01.ordinal()], messageFields[MsgFld.FIELD05.ordinal()]);
                     break;
                 case "AMG":
-                    some_other_method(messageFields[4], messageFields[17]);
+                    some_other_method(messageFields[MsgFld.FIELD04.ordinal()], messageFields[MsgFld.FIELD17.ordinal()]);
                     break;
                 case "OMG":
                     System.out.println("long code block for OMG");
